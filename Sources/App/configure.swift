62255a3d4901b41c0bff89d5a41d7776df0e8fbd
Tim <0xtimc@gmail.com>
2022-06-23 16:59:32 +0200

    Add postgres option for local dev

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -14,6 +14,14 @@ public func configure(_ app: Application) throws {
         postgresConfig.tlsConfiguration = .makeClientConfiguration()
         postgresConfig.tlsConfiguration?.certificateVerification = .none
         app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
     }
 
     app.migrations.add(CreateUser())
