4a7063aeaba4848136086ffb8a970daf60ff2525
Tim <0xtimc@gmail.com>
2022-07-24 10:22:42 +0100

    Move back to SQLite

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,5 +1,5 @@
 import Fluent
-import FluentPostgresDriver
+import FluentSQLiteDriver
 import Leaf
 import Vapor
 
@@ -10,19 +10,7 @@ public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.middleware.use(app.sessions.middleware)
 
-    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
-        postgresConfig.tlsConfiguration = .makeClientConfiguration()
-        postgresConfig.tlsConfiguration?.certificateVerification = .none
-        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
-    } else {
-        app.databases.use(.postgres(
-            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-        ), as: .psql)
-    }
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateUser())
     app.migrations.add(CreateWebAuthnCredential())
